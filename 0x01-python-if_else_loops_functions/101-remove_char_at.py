cb3d290404e62c01bd512b62c5535035a1f24ff7
Kelechi Egbuta <se.kaycee@gmail.com>
2022-11-29 16:27:36 +0100

    refactor 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if 0 <= n < len(str):
-        return (str.replace(str[n], ""))
-    return (str)
+    # this is the python way
+    # if 0 <= n < len(str):
+    #    return (str.replace(str[n], ""))
+    # return (str)
+
+    # the c in python way
+    s = ""
+    for i in range(len(str)):
+        if i != n:
+            s += str[i]
+    return (s)
