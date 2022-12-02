ae97c32ccf80a2a28a3f4ffad067c89c783e1772
Kelechi Egbuta <se.kaycee@gmail.com>
2022-12-02 14:06:25 +0100

    add 2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+    if idx > len(my_list) - 1:
+        return (my_list)
+
+    my_list[idx] = element
+    return (my_list)
