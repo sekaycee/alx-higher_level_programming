68dcb326694d058ad9767f6bac4e16ea435b889b
Kelechi Egbuta <se.kaycee@gmail.com>
2022-12-03 13:50:34 +0100

    refactor code

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,5 @@
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return (my_list)
-    if idx > len(my_list) - 1:
+    if idx < 0 or idx >= len(my_list):
         return (my_list)
 
     my_list[idx] = element
