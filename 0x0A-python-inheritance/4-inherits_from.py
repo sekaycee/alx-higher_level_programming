74539f584c2011293280b4063fe11ec829370f5b
Kay Cee <se.kaycee@gmail.com>
2023-01-09 16:17:18 +0100

    add 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    ''' return True/False if obj is an instance of a_class
+    Args:
+        obj: object
+        a_class: class type
+    Returns:
+        True if obj is an instance of a_class
+        False, otherwise
+    '''
+    return (type(obj) != a_class and isinstance(obj, a_class))
