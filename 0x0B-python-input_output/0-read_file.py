3acdec9382b574f5d03e244a740427df9b64d6dd
Kay Cee <se.kaycee@gmail.com>
2023-01-10 12:21:36 +0100

    add 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' Module with function that reads from a file '''
+
+
+def read_file(filename=""):
+    ''' read from a file
+    Args:
+        filename: filename
+    Raises
+        Exception: when the file can be opened
+    '''
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end='')
