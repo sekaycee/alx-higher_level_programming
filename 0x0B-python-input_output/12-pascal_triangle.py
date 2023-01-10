9b3bda425c34a8c5d8d60dfa92552203f64d072f
Kay Cee <se.kaycee@gmail.com>
2023-01-10 14:33:10 +0100

    add 12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' define a Pascal's Triangle module '''
+
+
+def pascal_triangle(n):
+    ''' represent Pascal's Triangle of size n
+    Args:
+        n: number of lines
+    Returns:
+        matrix: lists of integers representing the triangle
+    '''
+    matrix = [[1]]
+    while len(matrix) != n and n > 0:
+        tri = matrix[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        matrix.append(tmp)
+    return (matrix)
