e4528f90ae86dd6a69cdfa6f4ca9a2c735345e7f
Kay Cee <se.kaycee@gmail.com>
2023-01-12 15:50:35 +0100

    edit rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,6 @@ class Rectangle(Base):
     Inherits from:
         Base
     '''
-
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
