8148d2aa7010623cba827f058c12de99df35ed68
Kay Cee <se.kaycee@gmail.com>
2023-02-14 21:21:38 +0100

    add 5-full_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
 -- Create a table in the current database
 CREATE TABLE IF NOT EXISTS first_table (
-id INT,
-name VARCHAR(256));
+	id INT,
+	name VARCHAR(256)
+);
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Print full description of a table
+SHOW CREATE TABLE first_table;
