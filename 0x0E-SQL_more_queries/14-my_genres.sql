6353411a88d65d9b917eeab94b96dc9ad4bb8075
Kay Cee <se.kaycee@gmail.com>
2023-02-15 17:10:25 +0100

    add 14-my_genres.sql

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- List all shows contained in 'hbtn_0d_tvshows' without a genre linked
+SELECT tv_genres.name
+  FROM tv_genres
+ INNER JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+ INNER JOIN tv_shows
+    ON tv_show_genres.show_id = tv_shows.id
+ WHERE tv_shows.title = 'Dexter'
+ ORDER BY tv_genres.name;
