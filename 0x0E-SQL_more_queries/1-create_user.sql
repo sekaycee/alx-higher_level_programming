aaad3a65c7d165ef9d4e0d068475b4f18db176d3
Kay Cee <se.kaycee@gmail.com>
2023-02-15 16:23:22 +0100

    add 1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create the MySQL server user 'user_0d_1' and grant it all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
