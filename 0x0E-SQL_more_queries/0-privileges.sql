215c36e5941ed035d4538dcd22403c9cf3b72d78
Kay Cee <se.kaycee@gmail.com>
2023-02-15 16:43:29 +0100

    add 7-cities.sql and refactor style

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---  List all privileges of the MySQL users 'user_0d_1' and 'user_0d_2'
+--  List all privileges of MySQL users 'user_0d_1' and 'user_0d_2'
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
