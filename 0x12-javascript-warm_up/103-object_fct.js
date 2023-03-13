6aef0f658b9a13303c9a8c0c1e90fbf71bde65d5
Kay Cee <se.kaycee@gmail.com>
2023-03-13 23:13:47 +0000

    edit 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,14 +4,14 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
+
 myObject.incr = function () {
   this.value++;
 };
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
 
-
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
