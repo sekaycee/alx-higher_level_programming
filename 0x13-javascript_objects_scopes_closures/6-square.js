fc80fdc87539bbeafec3c9f8554691c12d558b86
Kay Cee <se.kaycee@gmail.com>
2023-03-14 17:52:10 +0000

    add 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
