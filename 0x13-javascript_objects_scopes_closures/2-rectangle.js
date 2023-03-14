6d126fc8a9135cfa0cee17467650c308e6f5c945
Kay Cee <se.kaycee@gmail.com>
2023-03-14 17:35:32 +0000

    add 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
