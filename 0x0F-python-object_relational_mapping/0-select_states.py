e8f682d858f793e59a6cf82ac16054cd9946fac5
Kay Cee <se.kaycee@gmail.com>
2023-03-17 16:24:53 +0100

    change port to 3306

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,9 +5,9 @@ import MySQLdb
 
 if __name__ == '__main__':
     conn = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], port=3600)
+                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states;")
+    cur.execute('SELECT * FROM states ORDER BY states.id ASC')
     states = cur.fetchall()
     for state in states:
         print(state)
