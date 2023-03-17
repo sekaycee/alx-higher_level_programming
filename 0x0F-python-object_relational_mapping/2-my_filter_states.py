7eea86c2182ab5d53253b32152d5b4d5a44ff88c
Kay Cee <se.kaycee@gmail.com>
2023-03-17 03:08:52 +0100

    add 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+''' List all states from the database hbtn_0e_0_usa with a name starting with
+    N (upper N)
+'''
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host='localhost',
+                           port=3600,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset='utf8')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM states WHERE name LIKE BINARY "%{}%" \
+        ORDER BY id ASC'.format(sys.argv[4])
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    cur.close()
+    conn.close()
