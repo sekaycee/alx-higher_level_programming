2ff66842d4ab5ae9aa87745da42fe82e32ceba04
Kay Cee <se.kaycee@gmail.com>
2023-03-19 14:47:18 +0000

    conform to pycodestyle

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,13 +4,13 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    conn = MySQLdb.connect(host='localhost', user=sys.argv[1], port=3306,
+                           passwd=sys.argv[2], db=sys.argv[3])
 
     cur = conn.cursor()
     cur.execute('SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.state_id = states.id \
-    WHERE states.name = "{}";'.format(sys.argv[4]))
+            FROM cities JOIN states ON cities.state_id = states.id \
+            WHERE states.name = "{}";'.format(sys.argv[4]))
 
     cities = cur.fetchall()
     print(", ".join([city[1] for city in cities]))
