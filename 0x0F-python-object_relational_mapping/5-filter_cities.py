0ba6a85e5730e5aee174b6c75e05b1db4e47c7be
Kay Cee <se.kaycee@gmail.com>
2023-03-19 14:28:50 +0000

    correct indentation to fit pycodestyle

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
 
     cur = conn.cursor()
     cur.execute('SELECT cities.id, cities.name, states.name \
-        FROM cities JOIN states ON cities.state_id = states.id \
-        WHERE states.name = "{}";'.format(sys.argv[4]))
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = "{}";'.format(sys.argv[4]))
 
     cities = cur.fetchall()
     print(", ".join([city[1] for city in cities]))
