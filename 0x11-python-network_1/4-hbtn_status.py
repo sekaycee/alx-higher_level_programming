d9dd4c71a87e8221b82f7c9796a14ed8514d1c80
Kay Cee <se.kaycee@gmail.com>
2023-04-01 00:50:34 +0100

    [Fix] move import declaration

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 ''' Python script that fetches an URL with requests package '''
-import requests
 
 
 if __name__ == '__main__':
+    import requests
+
     t = requests.get('https://intranet.hbtn.io/status').text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
