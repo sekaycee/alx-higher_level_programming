a3a15199475fea0603e0e3159d2aa6a8d2de3577
Kay Cee <se.kaycee@gmail.com>
2023-04-01 01:55:11 +0100

    grasping at strawd right now

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status """
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
 
-if __name__ == '__main__':
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         status = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(status)))
-        print("\t- status: {}".format(status))
-        print("\t- utf8 status: {}".format(status.decode('utf-8')))
+        print('Body response:')
+        print('\t- type: {}'.format(type(status)))
+        print('\t- content: {}'.format(status))
+        print('\t- utf8 content: {}'.format(status.decode("utf-8")))
