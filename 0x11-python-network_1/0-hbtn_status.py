b7e4dbcaf1f5833460bdb3bfa8c8a3d714bcfbdb
Kay Cee <se.kaycee@gmail.com>
2023-04-01 01:48:03 +0100

    [Fix] debug 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as res:
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         status = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(status)))
-        print("\t- content: {}".format(status))
-        print("\t- utf8 content: {}".format(status.decode("utf-8")))
+        print("\t- status: {}".format(status))
+        print("\t- utf8 status: {}".format(status.decode('utf-8')))
