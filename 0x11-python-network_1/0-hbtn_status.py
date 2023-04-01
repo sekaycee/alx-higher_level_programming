bf3a16e680f48b6eee5ab1321048589542a57706
Kay Cee <se.kaycee@gmail.com>
2023-04-01 01:58:23 +0100

    back to square one

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
+''' Python script that fetches https://intranet.hbtn.io/status '''
 import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        status = response.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        status = res.read()
         print('Body response:')
         print('\t- type: {}'.format(type(status)))
         print('\t- content: {}'.format(status))
-        print('\t- utf8 content: {}'.format(status.decode("utf-8")))
+        print('\t- utf8 content: {}'.format(status.decode('utf-8')))
