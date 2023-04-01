1ce5af40349da1cde4a23b32e1f75297aa92c51b
Kay Cee <se.kaycee@gmail.com>
2023-04-01 01:42:23 +0100

    edit 4-hbtn_status.py again

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-''' Python script that fetches an URL with requests package '''
+""" Fetch https://alx-intranet.hbtn.io/status """
 import requests
 
 
-if __name__ == '__main__':
-    res = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(res.text)))
-    print('\t- content: {}'.format(res.text))
+if __name__ == "__main__":
+    res = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
