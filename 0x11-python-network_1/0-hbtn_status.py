6fff67bb23742f3306d26f610be88c0531a7bea3
Kay Cee <se.kaycee@gmail.com>
2023-04-01 02:03:27 +0100

    [Fix] change import statement style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 ''' Python script that fetches https://intranet.hbtn.io/status '''
-import urllib.request
+from urllib import request
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+if __name__ == '__main__':
+    with request.urlopen('https://intranet.hbtn.io/status') as res:
         status = res.read()
         print('Body response:')
         print('\t- type: {}'.format(type(status)))
