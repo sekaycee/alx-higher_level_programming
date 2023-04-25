16ad7b37d7f6d7d40e53b7fd6138a8fc54a678bc
Kay Cee <se.kaycee@gmail.com>
2023-04-25 20:54:19 +0100

    feat: add 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const req = require('request');
+req.get(process.argv[2]).on(function (err, res, body) {
+  if (!err) {
+    const data = JSON.parse(body).results;
+    console.log(data.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1 : count;
+    }, 0));
+  }
+});
