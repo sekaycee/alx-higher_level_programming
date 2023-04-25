64aba8e2d5e7c1ce3decd983ba60b097ec67f2e8
Kay Cee <se.kaycee@gmail.com>
2023-04-25 20:55:32 +0100

    feat: add 101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+req.get(url).on('body', function (body) {
+  const characters = JSON.parse(body).characters;
+  printCharacters(characters, 0);
+});
+
+function printCharacters (characters, index) {
+  req.get(characters[index]).on('body', function (body) {
+    console.log(JSON.parse(body).name);
+    if (index++ < characters.length) {
+      printCharacters(characters, index++);
+    }
+  });
+}
